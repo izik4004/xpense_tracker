b69f7c69fc91c78ee84808ac6677ee1e16207591
izik4004 <izik4004@gmail.com>
2022-09-30 23:22:18 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,52 @@
-import {useState, useEffect} from 'react';
-import {header} from '../data'
-import {HiMenuAlt4, HiOutlinex} from "react-icons/hi"
-import MobileNav from '..components/MobileNav'
-import Nav from '../components/Nav'
-
+import { useState, useEffect } from "react";
+import { header } from "../data";
+import { HiMenuAlt4, HiAdjustments} from "react-icons/hi";
+import MobileNav from "../components/MobileNav";
+import Nav from "../components/Nav";
 
 const Header = () => {
-  const [mobileNav, setMobileNav] = useState(false)
-  const []
-  return <div>Header</div>;
+  const [mobileNav, setMobileNav] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const { logo, btnText } = header;
+
+  //scroll event
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        isActive ? "lg:top-0 bg-white shadow-2xl" : "lg;top-[60px]"
+      } py-6 lg:py-4 fixed w-full
+  transition-all z-10`}
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        <button className="btn btn-sm btn-outline hidden lg:flex">
+          {btnText}
+        </button>
+        <button className="lg:hidden" onClick={() => setMobileNav(!mobileNav)}>
+          {mobileNav ? (
+            <HiAdjustments className="text-3xl text-accent" />
+          ) : (
+            <HiMenuAlt4 className="text-3xl text-accent" />
+          )}
+        </button>
+
+        <div className={`${mobileNav ? 'left-0' : '-left-full'} fixed top-0 w-[60vw] lg:hidden transition-all bottom-0 bg-pink-400`}>
+          <MobileNav/>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
