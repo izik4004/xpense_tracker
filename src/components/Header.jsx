ab12d57451d334470b9208e49844ef1bea8d689e
izik4004 <izik4004@gmail.com>
2022-10-01 00:04:48 +0100

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { header } from "../data";
-import { HiMenuAlt4, HiAdjustments} from "react-icons/hi";
+import { HiMenuAlt4} from "react-icons/hi";
+import {MdClose} from "react-icons/md"
 import MobileNav from "../components/MobileNav";
 import Nav from "../components/Nav";
 
@@ -24,25 +25,37 @@ const Header = () => {
   transition-all z-10`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        <a href="#">
+        <a href="#"
+        data-aos='fade-down'
+        data-aos-delay='1200'
+        >
           <img src={logo} alt="" />
         </a>
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex"
+        data-aos='fade-down'
+        data-aos-delay='1200'>
           <Nav />
         </div>
-        <button className="btn btn-sm btn-outline hidden lg:flex">
+        <button className="btn btn-sm btn-outline hidden lg:flex"
+        data-aos='fade-down'
+        data-aos-delay='1400'
+        >
           {btnText}
         </button>
         <button className="lg:hidden" onClick={() => setMobileNav(!mobileNav)}>
           {mobileNav ? (
-            <HiAdjustments className="text-3xl text-accent" />
+            <MdClose className="text-3xl text-accent" />
           ) : (
             <HiMenuAlt4 className="text-3xl text-accent" />
           )}
         </button>
 
-        <div className={`${mobileNav ? 'left-0' : '-left-full'} fixed top-0 w-[60vw] lg:hidden transition-all bottom-0 bg-pink-400`}>
-          <MobileNav/>
+        <div
+          className={`${
+            mobileNav ? "left-0" : "-left-full"
+          } fixed top-0 w-[60vw] lg:hidden transition-all bottom-0 bg-pink-400`}
+        >
+          <MobileNav />
         </div>
       </div>
     </header>
