7c23583672eae8d030a8f9792ed86ab7dd32888d
izik4004 <izik4004@gmail.com>
2022-10-02 00:01:32 +0100

    features

--- src/components/Feature1.jsx
+++ src/components/Feature1.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import {features} from "../data"
+import { features } from "../data";
 
 const Feature1 = () => {
-  const {feature1} = features;
+  const { feature1 } = features;
 
-  const {pretitle, title, subtitle, btnLink, btnIcon} = feature1
+  const { pretitle, title, image, subtitle, btnLink, btnIcon } = feature1;
   return (
-    section
-  <div>Feature1</div>
-  )
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:dap-x-[30px]">
+          <div className="flex-1"
+          data-aos='fade-up'
+          data-aos-offset='400'>
+            <div className="pretitle">{pretitle}</div>
+            <h2 className="title">{title}</h2>
+            <p className="lead">{subtitle}</p>
+            <button className="btn-link flex items-center gap-x-3 hover:gap-x-5 transition-all">
+              {btnLink}
+              <img src={btnIcon} alt="" />
+            </button>
+          </div>
+          <div className="flex-1"
+          data-aos='fade-left'
+          data-aos-offset='300'
+          >
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature1;
--- src/components/Feature2.jsx
+++ src/components/Feature2.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+
+import { features } from "../data";
 
 const Feature2 = () => {
-  return <div>Feature2</div>;
+  const { feature2} = features;
+
+  const { pretitle, title, image, subtitle, btnLink, btnIcon } = feature2;
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:dap-x-[30px]">
+          <div className="flex-1 order-1 lg:order-2"
+          data-aos='fade-right'
+          data-aos-offset='400'>
+            <div className="pretitle">{pretitle}</div>
+            <h2 className="title">{title}</h2>
+            <p className="lead">{subtitle}</p>
+            <button className="btn-link flex items-center gap-x-3 hover:gap-x-5 transition-all">
+              {btnLink}
+              <img src={btnIcon} alt="" />
+            </button>
+          </div>
+          <div className="flex-1 order-2 lg:order-1"
+          data-aos='fade-left'
+          data-aos-offset='300'
+          >
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature2;
